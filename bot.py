464112202c471a0f8de40f79b371aa4bc983fdb1
sfsmikroskil <sfsmikroskil@gmail.com>
2026-09-01 22:03:58 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ async def main(nama, email, c):
         #Live chat
         await page.mouse.click(360, 669)
         await page.wait_for_timeout(2000)
-        print(f"Akun : ({c}) {nama} | Selesai")
+        print(f"Akun : ({c+1}) {nama} | Selesai")
         await browser.close()
 
 if __name__ == "__main__":
