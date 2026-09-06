1692629acdee21564dac2750f0bb0d598c4ba157
sfsmikroskil <sfsmikroskil@gmail.com>
2026-09-06 18:04:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
         nama = contact["nama"]
         #nama = nama[3:-3]
         email = contact["email"]
-        email = sisip(email, ".14)
+        email = sisip(email, ".14")
         c = i
         #print(f"Proses: {nama} ({email})")
         asyncio.run(main(nama, email, c))
